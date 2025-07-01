4f2e36655d05866d6ab7ad9d17dcb5128f293cc8
Samantha Wallis <165694247+samanthawallis@users.noreply.github.com>
2025-07-01 14:32:27 +0100

    update pull_data.py for lobster data

--- data/pull_data.py
+++ data/pull_data.py
@@ -2,17 +2,16 @@ import os
 from typing import List
 
 import pandas as pd
-import yfinance as yf
 
 import numpy as np
 
 from settings.default import PINNACLE_DATA_CUT, PINNACLE_DATA_FOLDER
 
-def pull_quandl_sample_data(ticker: str) -> pd.DataFrame:
+def pull_lobster_data(ticker: str) -> pd.DataFrame:
     return (
-        pd.read_csv(os.path.join("data", "quandl", f"{ticker}.csv"), parse_dates=[0])
-        .rename(columns={"Trade Date": "date", "Date": "date", "Settle": "close"})
-        .set_index("date")
+        pd.read_parquet(os.path.join("processed", "AAPL", "prices.parquet"))[['datetime', 'mid_price']]
+        .rename(columns={'datetime': 'date'})
+        .set_index('date')
         .replace(0.0, np.nan)
     )
 
